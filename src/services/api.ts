7b6c944c9a590d5f97bbf5a7b094112b9a4b2069
Amina Bajrić <115504860+abajric2@users.noreply.github.com>
2024-05-10 20:53:46 +0200

    Api methods refactoring

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,11 @@
 import axios, { AxiosRequestConfig } from 'axios';
-import { TvShow, TvShowsResponse } from '../types/TVShowTypes';
-import { Movie, MovieResponse } from '../types/MovieTypes';
+import { TvShow } from '../types/TVShowTypes';
+import { Movie } from '../types/MovieTypes';
+import {Media} from '../types/Media'
+import { MediaResponse } from '../types/MediaResponse';
 import { Genre } from '../types/Genre';
 import { Trailer } from '../types/Trailer';
+import { MediaType } from '../types/MediaType';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = '18d8131ff2d912b3171edc9bd7f4c492';
@@ -17,7 +20,7 @@ const fetchData = async <T>(config: AxiosRequestConfig): Promise<T> => {
     }
 };
 
-const fetchGenres = async (mediaType: 'movie' | 'tv'): Promise<Genre[]> => {
+const fetchGenres = async (mediaType: MediaType): Promise<Genre[]> => {
     const url = `${API_BASE_URL}/genre/${mediaType}/list?language=en-US&api_key=${API_KEY}`;
     const config: AxiosRequestConfig = { url };
     const data = await fetchData<{ genres: Genre[] }>(config);
@@ -29,10 +32,7 @@ const fetchGenres = async (mediaType: 'movie' | 'tv'): Promise<Genre[]> => {
     return data.genres;
 };
 
-const mapGenreIdsToStrings = async <T extends { id: number; genre_ids: number[] }>(
-    mediaType: 'movie' | 'tv',
-    items: T[]
-): Promise<T[]> => {
+const mapGenreIdsToStrings = async <T extends { id: number; genre_ids: number[] }>(mediaType: MediaType, items: T[]): Promise<T[]> => {
     const genres = await fetchGenres(mediaType);
     return items.map(item => {
         const genreObjects = item.genre_ids.map(id => {
@@ -43,59 +43,55 @@ const mapGenreIdsToStrings = async <T extends { id: number; genre_ids: number[] 
     });
 };
 
-export const fetchTvShows = async (): Promise<TvShow[]> => {
-    const url = `${API_BASE_URL}/tv/top_rated?language=en-US&page=1&api_key=${API_KEY}`;
+const fetchMedia = async <T extends Media>(mediaType: MediaType): Promise<T[]> => {
+    const url = `${API_BASE_URL}/${mediaType}/top_rated?language=en-US&page=1&api_key=${API_KEY}`;
     const config: AxiosRequestConfig = { url };
-    const data = await fetchData<TvShowsResponse>(config);
+    const data = await fetchData<MediaResponse<T>>(config);
 
     if (!data || !data.results) {
         throw new Error('Invalid data format received from API');
     }
 
-    const tvShows = await mapGenreIdsToStrings('tv', data.results.slice(0, 10));
+    return data.results;
+};
+
+export const fetchTvShows = async (): Promise<TvShow[]> => {
+    const results = await fetchMedia<TvShow>('tv') as TvShow[];
+    const tvShows = await mapGenreIdsToStrings('tv', results.slice(0, 10));
     return tvShows;
 };
 
 export const fetchMovies = async (): Promise<Movie[]> => {
-    const url = `${API_BASE_URL}/movie/top_rated?language=en-US&page=1&api_key=${API_KEY}`;
-    const config: AxiosRequestConfig = { url };
-    const data = await fetchData<MovieResponse>(config);
-
-    if (!data || !data.results) {
-        throw new Error('Invalid data format received from API');
-    }
-
-    const movies = await mapGenreIdsToStrings('movie', data.results.slice(0, 10));
+    const results = await fetchMedia<Movie>('movie') as Movie[];
+    const movies = await mapGenreIdsToStrings('movie', results.slice(0, 10));
     return movies;
 };
 
-export const searchMovies = async (query: string): Promise<Movie[]> => {
-    const url = `${API_BASE_URL}/search/movie?query=${query}&include_adult=false&language=en-US&page=1&api_key=${API_KEY}`;
+const searchMedia = async <T extends Media>(mediaType: MediaType, query: string): Promise<T[]> => {
+    const url = `${API_BASE_URL}/search/${mediaType}?query=${query}&include_adult=false&language=en-US&page=1&api_key=${API_KEY}`;
     const config: AxiosRequestConfig = { url };
-    const data = await fetchData<MovieResponse>(config);
+    const data = await fetchData<MediaResponse<T>>(config);
 
     if (!data || !data.results) {
         throw new Error('Invalid data format received from API');
     }
 
-    const movies = await mapGenreIdsToStrings('movie', data.results);
+    return data.results
+};
+
+export const searchMovies = async (query: string): Promise<Movie[]> => {
+    const results = await searchMedia<Movie>('movie', query) as Movie[]
+    const movies = await mapGenreIdsToStrings('movie', results);
     return movies;
 };
 
 export const searchTVShows = async (query: string): Promise<TvShow[]> => {
-    const url = `${API_BASE_URL}/search/tv?query=${query}&include_adult=false&language=en-US&page=1&api_key=${API_KEY}`;
-    const config: AxiosRequestConfig = { url };
-    const data = await fetchData<TvShowsResponse>(config);
-
-    if (!data || !data.results) {
-        throw new Error('Invalid data format received from API');
-    }
-
-    const tvShows = await mapGenreIdsToStrings('tv', data.results);
+    const results = await searchMedia<TvShow>('tv', query) as TvShow[]
+    const tvShows = await mapGenreIdsToStrings('tv', results);
     return tvShows;
 };
 
-const fetchVideosById = async (id: number, mediaType: 'movie' | 'tv'): Promise<string | null> => {
+const fetchVideosById = async (id: number, mediaType: MediaType): Promise<string | null> => {
     const url = `${API_BASE_URL}/${mediaType}/${id}/videos?language=en-US&api_key=${API_KEY}`;
     const config: AxiosRequestConfig = { url };
     const data = await fetchData<{ results: Trailer[] }>(config);
@@ -109,16 +105,16 @@ const fetchVideosById = async (id: number, mediaType: 'movie' | 'tv'): Promise<s
     return null;
 };
 
-export const fetchMovieById = async (id: number): Promise<Movie | null> => {
-    const url = `${API_BASE_URL}/movie/${id}?language=en-US&api_key=${API_KEY}`;
+const fetchMediaById = async <T extends Media>(mediaType: MediaType, id: number): Promise<T | null> => {
+    const url = `${API_BASE_URL}/${mediaType}/${id}?language=en-US&api_key=${API_KEY}`;
     const config: AxiosRequestConfig = { url };
-    const data = await fetchData<Movie>(config);
+    const data = await fetchData<T>(config);
 
     if (!data) {
         return null;
     }
 
-    const trailer = await fetchVideosById(id, 'movie');
+    const trailer = await fetchVideosById(id, `${mediaType}`);
     data.trailer = trailer;
 
     if (data.genres) {
@@ -129,23 +125,13 @@ export const fetchMovieById = async (id: number): Promise<Movie | null> => {
     return data;
 };
 
+export const fetchMovieById = async (id: number): Promise<Movie | null> => {
+    const data = await fetchMediaById<Movie>('movie', id) as Movie | null;
+    return data;
+};
+
 export const fetchTVShowById = async (id: number): Promise<TvShow | null> => {
-    const url = `${API_BASE_URL}/tv/${id}?language=en-US&api_key=${API_KEY}`;
-    const config: AxiosRequestConfig = { url };
-    const data = await fetchData<TvShow>(config);
-
-    if (!data) {
-        return null;
-    }
-
-    const trailer = await fetchVideosById(id, 'tv');
-    data.trailer = trailer;
-
-    if (data.genres) {
-        const genres: Genre[] = data.genres.map(genre => ({ id: genre.id, name: genre.name }));
-        data.genres = genres;
-    }
-
+    const data = await fetchMediaById<TvShow>('tv', id) as TvShow | null;
     return data;
 };
 

5d1984507703dcfec68b797aa2bbcce00404c5cd
Amina Bajrić <115504860+abajric2@users.noreply.github.com>
2024-05-10 03:35:59 +0200

    Movie grid item design changed

--- src/components/MovieGridItem/MovieGridItem.tsx
+++ src/components/MovieGridItem/MovieGridItem.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Movie } from '../../types/MovieTypes';
 import { Genre } from '../../types/Genre';
-import './MovieGridItem.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import './MovieGridItem.css';
 
 interface MovieGridItemProps {
   movie: Movie;
@@ -13,10 +15,20 @@ const MovieGridItem: React.FC<MovieGridItemProps> = ({ movie }) => {
 
   return (
     <>
-      <Link to={`/movie/${movie.id}`} className="movie-grid-item">
+      <Link to={`/movie/${movie.id}`} className="movie-grid-item" style={{ textDecoration: 'none' }}>
         <img src={posterUrl} alt={movie.title} />
         <h3>{movie.title}</h3>
-        <p>Genres: {movie?.genres.map((genre: Genre) => genre.name).join(', ')}</p>
+        <div className="genres-and-rating">
+          <div className="vote-average">
+            <FontAwesomeIcon icon={faStar} style={{color: "#FFD43B"}} />
+            <p>{movie.vote_average}</p>
+          </div>
+          <div className="genres">
+            {movie?.genres.map((genre: Genre, index: number) => (
+              <p key={index}>{genre.name}</p>
+            ))}
+          </div>
+        </div>
       </Link>
     </>
   );

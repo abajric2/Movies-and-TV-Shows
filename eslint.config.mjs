9a24950e64c3a87b4ad19311fe68e9a912323872
Amina Bajrić <115504860+abajric2@users.noreply.github.com>
2024-05-11 03:21:29 +0200

    Eslint configuration changed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,23 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
+const reactSettings = {
+  settings: {
+    react: {
+      version: "detect"
+    }
+  }
+};
 
 export default [
   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReactConfig,
+  {
+    ...pluginReactConfig,
+    ...reactSettings
+  },
+  {
+    ignores: ["build/**/*"]
+  },
 ];
